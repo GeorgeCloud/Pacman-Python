c696fd7e3c5de213876a01b847751e66cfd1a577
George Ceja <Net.george.sky@gmail.com>
2021-11-17 12:39:07 -0800

    pacman running

--- cookie_cutter.py
+++ cookie_cutter.py
@@ -1,15 +1,72 @@
 import pygame
 from random import randint
 
+image_names = ['1.png', '2.png', '3.png']
+
+pacman_left  = ['pacman-art/pacman-left/' + i for i in image_names]
+pacman_right = ['pacman-art/pacman-right/' + i for i in image_names]
+pacman_up    = ['pacman-art/pacman-up/' + i for i in image_names]
+pacman_down  = ['pacman-art/pacman-down/' + i for i in image_names]
+
+facing_positions = [pacman_left, pacman_right, pacman_up, pacman_down]
+
 class GameObject(pygame.sprite.Sprite):
-    def __init__(self, x, y, image):
+    def __init__(self, x, y, image_path):
         super(GameObject, self).__init__()
-        resized_image = pygame.image.load(image)
-        self.surf = pygame.transform.scale(resized_image, (30, 30))
+        self.surf = None
+        self.update_image(image_path)
         self.x = x
         self.y = y
 
+    def update_image(self, image_path):
+        resized_image = pygame.image.load(image_path)
+        self.surf = pygame.transform.scale(resized_image, (30, 30))
+
     def render(self, screen):
+        screen.blit(self.surf, (self.x, self.y))
+
+class Player(GameObject):
+    player_speed = 30
+
+    def __init__(self):
+        super(Player, self).__init__(250, 250, 'pacman-art/pacman-right/1.png')
+        self.x = 250
+        self.y = 250
+        self.current_direction = 1
+        self.img_idx = 0
+
+    def move_left(self):
+        self.x -= self.player_speed
+        if self.x < 0:
+            self.x = 470
+
+        self.current_direction = 0
+
+    def move_right(self):
+        self.x += self.player_speed
+        if self.x >= 500:
+            self.x = 0
+
+        self.current_direction = 1
+
+    def move_up(self):
+        self.y -= self.player_speed
+        if self.y < 0:
+            self.y = 470
+
+        self.current_direction = 2
+
+    def move_down(self):
+        self.y += self.player_speed
+        if self.y >= 500:
+            self.y = 0
+
+        self.current_direction = 3
+
+    def render(self, screen):
+        current_direction = facing_positions[self.current_direction]
+        self.update_image(current_direction[self.img_idx])
+        self.img_idx += -2 if self.img_idx == 2 else 1
         screen.blit(self.surf, (self.x, self.y))
 
 
@@ -49,3 +106,22 @@ class Strawberry(GameObject):
     def reset(self):
         self.x = -64
         self.y = randint(0, 470)
+
+
+class Ghost(GameObject):
+    def __init__(self):
+        super(Ghost, self).__init__(0, 0, 'pacman-art/ghosts/inky.png')
+        self.dx = 0
+        self.dy = (randint(0, 200) / 100) + 1
+        self.reset()
+
+    def move(self):
+        self.x += self.dx
+        self.y += self.dy
+
+        if self.y > 500:
+            self.reset()
+
+    def reset(self):
+        self.x = randint(0, 470)
+        self.y = -64
--- main.py
+++ main.py
@@ -1,34 +1,31 @@
 import pygame
 from random import randint, shuffle
-from cookie_cutter import GameObject, Apple, Strawberry
+from cookie_cutter import GameObject, Apple, Ghost, Player, Strawberry
 from colors import colors
-
-
-screen = pygame.display.set_mode([500, 500])
-clock = pygame.time.Clock()
-
-screen.fill(colors['black'])
-
-pacman = GameObject(225, 225, 'pacman-art/pacman-right/1.png')
 
 def random_coordinates():
     return randint(0, 400), randint(0, 400)
 
 
+screen = pygame.display.set_mode([500, 500])
+screen.fill(colors['black'])
+
+clock = pygame.time.Clock()
+
+some_apple, some_strawberry = Apple(), Strawberry()
+
+ghost = Ghost()
+
+player = Player()
+
 fruits = []
+
+points = 0
 
 for _ in range(3):
     x, y = random_coordinates()  # Add error collision
     fruits.append(Apple())
     fruits.append(Strawberry())
-
-apple = GameObject(250, 225, 'pacman-art/other/apple.png')
-
-strawberry = GameObject(275, 225, 'pacman-art/other/strawberry.png')
-
-some_apple = Apple()
-
-some_strawberry = Strawberry()
 
 running = True
 while running:
@@ -36,19 +33,42 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             break
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+            elif event.key == pygame.K_LEFT:
+                player.move_left()
+                # player.change_image()
+            elif event.key == pygame.K_RIGHT:
+                player.move_right()
+            elif event.key == pygame.K_UP:
+                player.move_up()
+            elif event.key == pygame.K_DOWN:
+                player.move_down()
 
     # Clear the screen
     screen.fill(colors['black'])
+
+    print(f'x: {player.x}, y: {player.y}')
+    player.render(screen)
 
     for item in fruits:
         item.render(screen)
         item.move()
 
+    ghost.render(screen)
+    ghost.move()
+
     # Update the display
     pygame.display.flip()
     clock.tick(15)
 
+pygame.mixer.pre_init(frequency=44100, size=-16, channels=1, buffer=512)
 pygame.init()
+
+pygame.mixer.music.load('eating.mp3')
+pygame.mixer.music.set_volume(0.2)
+pygame.mixer.music.play(5)
 
 # # Draw a circle
 # starting_position = [100, 100]
