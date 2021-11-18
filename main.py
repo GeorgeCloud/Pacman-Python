474a2f7bd0627e537bbece1e6365a30bc9ac9f77
George Ceja <Net.george.sky@gmail.com>
2021-11-18 14:23:05 -0800

    clean up

--- main.py
+++ main.py
@@ -4,11 +4,6 @@ from time import sleep
 from cookie_cutter import Apple, Ghost, Player, Strawberry
 from colors import colors
 
-
-def random_coordinates():
-    return randint(0, 400), randint(0, 400)
-
-
 screen = pygame.display.set_mode([500, 500])
 screen.fill(colors['black'])
 
@@ -25,12 +20,12 @@ eating_sound_effect.play(-1)
 player = Player()
 
 all_ghosts = []
-ghost_sprites = pygame.sprite.Group()
-
 fruits = []
+ghost_sprites = pygame.sprite.Group()
 fruit_sprites = pygame.sprite.Group()
-every_three_renders = 0
 
+every_three_renders = 0
+# Create list of items to populate screen
 for _ in range(20):
     if every_three_renders > 3:
         ghost = Ghost()
@@ -48,7 +43,6 @@ for _ in range(20):
 
     every_three_renders += 1
 
-frame = 0
 running = True
 while running:
     for event in pygame.event.get():
@@ -60,7 +54,6 @@ while running:
                 running = False
             elif event.key == pygame.K_LEFT:
                 player.move_left()
-                # player.change_image()
             elif event.key == pygame.K_RIGHT:
                 player.move_right()
             elif event.key == pygame.K_UP:
@@ -70,7 +63,6 @@ while running:
 
     screen.fill(colors['black'])
 
-    print(f'x: {player.x}, y: {player.y}')
     player.render(screen)
 
     for item in fruits:
@@ -104,20 +96,3 @@ sleep(1.3)
 print("SCORE:", score)
 
 pygame.init()
-
-# # Draw a circle
-# starting_position = [100, 100]
-# GameObject(starting_position[0], starting_position[1], 'pacman-art/other/apple.png').render(screen)
-# # Challenge 2
-# for item_count in range(1, 9):
-#     if item_count % 3 == 0:
-#         starting_position[1] += 150
-#         starting_position[0] = -50
-#
-#     starting_position[0] += 150
-#
-#     if item_count % 2 == 0:
-#         GameObject(starting_position[0], starting_position[1], 'pacman-art/other/apple.png').render(screen)
-#     else:
-#         GameObject(starting_position[0], starting_position[1], 'pacman-art/other/strawberry.png').render(screen)
-#     item_count += 1
